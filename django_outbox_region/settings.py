545f12a51eef6b489dd8cc40b649f060a0c9a75a
Ione03 <suratiwan03@gmail.com>
2022-10-20 08:21:08 +0800

    Add template import-export library

--- django_outbox_region/settings.py
+++ django_outbox_region/settings.py
@@ -128,3 +128,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#STATIC_ROOT = BASE_DIR/ 'staticfiles' # 'staticfiles'  # sesuai contoh (apa efeknya blm ketahuan, mungkin di server)
+
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'static',
+#]
+
+#MEDIA_URL = '/media/'
+
+#MEDIA_ROOT = BASE_DIR / 'media'
